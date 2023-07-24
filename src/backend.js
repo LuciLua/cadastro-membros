a54ba73db6bce7c0825a345ab9f4ed97f679a4ac
Luci Lua <luci.lua81@gmail.com>
2023-07-24 16:57:24 -0300

    new app: cadastro de membros

---
+++ src/backend.js
@@ -0,0 +1,80 @@
+const express = require('express')
+const app = express()
+const port = '3000'
+
+const fs = require('fs')
+
+app.listen(port, (req, res) => {
+  console.log(`⌛ Listening to port ${port}...`)
+})
+
+// Add the following middleware to enable CORS
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
+app.get('/membros', (req, res) => {
+  fs.readFile('./public/membros.json', 'utf-8', (err, data) => {
+    if (err) { console.error('erro ao ler arquivo', err); return res.status(500).json({ error: "erro ao ler arquivo" }); }
+    const membrosList = JSON.parse(data)
+    return res.status(200).send(membrosList)
+  });
+})
+
+app.get('/functions', (req, res) => {
+  fs.readFile('./public/functions_list.json', 'utf-8', (err, data) => {
+    if (err) { console.error('erro ao ler arquivo', err); return res.status(500).json({ error: "erro ao ler arquivo" }); }
+    const functions_list = JSON.parse(data)
+    return res.status(200).send(functions_list)
+  });
+})
+
+let currentId = 1
+
+app.post('/membro', (req, res) => {
+  let membro_submited_by_form = req.query
+  const id = currentId++
+  let novo = JSON.parse(membro_submited_by_form)
+  novo.push("aaa")
+  novo = JSON.stringify(novo)
+  membro_submited_by_form = novo
+
+  if (!membro_submited_by_form || typeof membro_submited_by_form !== 'object') res.status(400).json({ error: "Query com parametros inválidos" })
+  fs.readFile('./public/membros.json', 'utf-8', (err, data) => {
+    if (err) { console.error('erro ao ler o arquivo', err); return res.status(500).json({ error: 'erro ao ler o arquivo' }) }
+    try {
+      const json_membros_to_object = JSON.parse(data)
+      json_membros_to_object.membros.push(membro_submited_by_form)
+      const object_membros_to_json = JSON.stringify(json_membros_to_object, null, 2)
+
+      fs.writeFile('./public/membros.json', object_membros_to_json, 'utf-8', (err) => {
+        if (err) { console.error('erro ao escrever no arquivo', err); return res.status(500).json({ error: 'erro ao escrever no arquivo' }) }
+        console.log('Arquivo escrito com sucesso!'); return res.status(200).json({ message: 'Arquivo escrito com sucesso!' })
+      })
+    } catch (parseError) { console.error('erro no parse', parseError); return res.status(500).json({ error: 'erro no parse' }) }
+  })
+});
+
+// app.put('/membros', (req, res) => {
+//   const membro_submited_by_form = req.query
+//   if (!membro_submited_by_form || typeof membro_submited_by_form !== 'object') res.status(400).json({ error: "Query com parametros inválidos" })
+
+//   fs.readFile('./public/membros.json', 'utf-8', (err, data) => {
+//     if (err) { console.error('erro ao ler o arquivo', err); return res.status(500).json({ error: 'erro ao ler o arquivo' }) }
+//     try {
+//       const json_membros_to_object = JSON.parse(data)
+//       json_membros_to_object.membros.push(membro_submited_by_form)
+//       const object_membros_to_json = JSON.stringify(json_membros_to_object, null, 2)
+
+//       fs.writeFile('./public/membros.json', object_membros_to_json, 'utf-8', (err) => {
+//         if (err) { console.error('erro ao escrever no arquivo', err); return res.status(500).json({ error: 'erro ao escrever no arquivo' }) }
+//         console.log('Arquivo escrito com sucesso!'); return res.status(200).json({ message: 'Arquivo escrito com sucesso!' })
+//       })
+//     } catch (parseError) { console.error('erro no parse', parseError); return res.status(500).json({ error: 'erro no parse' }) }
+//   })
+// });
+
+
---
+++ src/main.js
@@ -0,0 +1,78 @@
+export async function fetchMembers(url) {
+    const membros = await fetch(url)
+        .then(resp => resp.json())
+        .then(json => json.membros)
+        .then(membros => {
+            return membros
+        })
+    return membros
+}
+export async function fetchFunctions(url) {
+    const functions = await fetch(url)
+        .then(resp => resp.json())
+        .then(json => json.functions)
+        .then(functions => {
+            return functions
+        })
+    return functions
+}
+async function insertFunctionsInOptions() {
+    const functions = await fetchFunctions('http://localhost:3000/functions')
+    const form_2 = document.getElementById("form2")
+    const select_functions_class = form_2.querySelectorAll("select.functions")
+    const select_functions_pos = form_2.querySelector("select#pos_producao")
+
+    let funcs_pos = []
+    let funcs_not_pos = []
+    functions.map(func => { func.pos ? funcs_pos.push(func) : funcs_not_pos.push(func) }) // coloca pos e not pos nos []
+
+    select_functions_class.forEach(select => {
+        funcs_not_pos.map(func_not_pos => { select.innerHTML += `<option value=${func_not_pos.name}>${func_not_pos.name}</option>` })
+    })
+
+    funcs_pos.map(funcs_pos => { select_functions_pos.innerHTML += `<option value=${funcs_pos.name}>${funcs_pos.name}</option>` })
+}
+insertFunctionsInOptions()
+async function insertMembersInOptions() {
+    const members = await fetchMembers('http://localhost:3000/membros')
+    const form_2 = document.getElementById("form2")
+    const select_names = form_2.querySelector("select#name")
+
+    members.map(member => {
+        select_names.innerHTML += `<option value=${member.fullname}>${member.fullname}</option>`
+    })
+}
+insertMembersInOptions()
+
+document.getElementById('form1').addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    // Criar o objeto FormData para coletar todos os campos do formulário
+    const formData = new FormData(event.target);
+
+    // Converter o objeto FormData para um objeto JavaScript simples
+    const data = {};
+    formData.forEach((value, key) => {
+      data[key] = value;
+    });
+
+    // Criar uma URL com os parâmetros do formulário
+    const urlParams = new URLSearchParams(data).toString();
+    const url = `http://localhost:3000/membros?${urlParams}`;
+
+    // Enviar a requisição POST usando Fetch com a URL criada
+    fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then(response => response.json())
+      .then(result => {
+        console.log(result); // Resposta da API após a atualização do arquivo
+        // Faça algo com a resposta, se necessário
+      })
+      .catch(error => {
+        console.error('Erro ao enviar a requisição:', error);
+      });
+  });
